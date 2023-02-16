21d3f545ab6442fa5359e1f980559ceb3f41cd40
Pauloslemma <pauloslemma1994@gmail.com>
2023-02-16 12:11:31 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+i#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\"");
+	fprintf(stderr, "%s", " - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
