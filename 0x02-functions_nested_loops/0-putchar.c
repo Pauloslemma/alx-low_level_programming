e8481a7684e9f509cd354cf0deccf5636cd0475c
Pauloslemma <pauloslemma1994@gmail.com>
2023-02-21 05:10:29 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,19 @@
-#include <stdio.h>
-int main()
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-	return 0;
-  
-
-
+	return (0);
 }
