3260f612b61a247751e17fa2a73c6470bec7f619
Pauloslemma <pauloslemma1994@gmail.com>
2023-02-21 12:49:27 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - print the sum of all mutiples of 3 or 5 up to 1024
+ * Return: Always (success)
+ */
 int main(void)
 {
-    int sum = 0;
-    for (int i = 0; i < 1024; i++) {
-        if (i % 3 == 0 || i % 5 == 0) {
-            sum += i;
-        }
-    }
-    printf("%d\n", sum);
-    return 0;
-}
+	int i, z = 0;
 
+	while (i < 1024)
+	{
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+	z += i;
+	}
+	i++;
+	}
+	printf( \n % d", z );
+	return (0);
+}
