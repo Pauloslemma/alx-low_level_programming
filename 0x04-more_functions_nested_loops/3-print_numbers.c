637813f691705b9f6b5201bc3b9471e19c454432
Pauloslemma <pauloslemma1994@gmail.com>
2023-02-23 09:21:01 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,9 +7,10 @@
 */
 void print_numbers(void)
 {
-int i;
+    char numbers[] = "0123456789\n";
+    int i;
 
-for (i = 0; i <= 9; i++)
-_putchar(i + '0');
-_putchar('\n');
+    for (i = 0; i <= 10; i++)
+        _putchar(numbers[i]);
 }
+
