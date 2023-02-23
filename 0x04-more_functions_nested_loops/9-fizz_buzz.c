03b72151f3b272fa9b27f10adbbd2353cd5b32a2
Pauloslemma <pauloslemma1994@gmail.com>
2023-02-23 11:14:46 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,18 @@
 #include "main.h"
 
 /**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+/**
 * print_square - a function that prints a square, followed by a new line
 * @size: size of both width and length
 * Return: a square made of '#'
@@ -26,3 +38,4 @@ _putchar('\n');
 }
 }
 }
+
