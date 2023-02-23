2adfc34b72c2473b2a2cbe81b87b5d4a1d497245
Pauloslemma <pauloslemma1994@gmail.com>
2023-02-23 03:21:05 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * print_square - a function that prints a square, followed by a new line
@@ -24,4 +24,4 @@ void print_square(int size)
 			}
 			_putchar('\n');
 		}
-	
+
