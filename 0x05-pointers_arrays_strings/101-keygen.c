8d8a89a935eb7dfa79accefadc2ef16012e4706c
Pauloslemma <pauloslemma1994@gmail.com>
2023-02-27 02:37:26 -0800

    hot fix

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 12
+
+int main(void)
+{
+char password[PASSWORD_LENGTH + 1];
+const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+int i;
+
+srand(time(NULL));
+
+for (i = 0; i < PASSWORD_LENGTH; i++) {
+password[i] = charset[rand() % (sizeof(charset) - 1)];
+}
+
+password[PASSWORD_LENGTH] = '\0';
+
+printf("%s", password);
+
+return 0;
+}
+
