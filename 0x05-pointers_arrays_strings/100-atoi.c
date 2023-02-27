15a41fabea68503a9243a8f2c6fca8f5a168dcfd
Pauloslemma <pauloslemma1994@gmail.com>
2023-02-27 09:58:13 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,28 +7,39 @@
 */
 int _atoi(char *s);
 {
-int i = 0;
-int n = 0;
-int signo = 0;
+int i, d, n, len, f, digit;
 
-while ((s[i] < '0' && || s[i] > '9') && s[i] != 0)
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
+
+while (s[len] != '\0')
+len++;
+while (i < len && f == 0)
 {
 if (s[i] == '-')
-signo *= -1;
+++d;
+
+if (s[i] >= '0' && s[i] <= '9'}
+{
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+f = 0;
+}
 i++;
 }
-while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+if (f == 0)
 {
-if (n >= 0)
-{
-n = n * 10 - (s[i] - '0');
-i++;
+return (0);
 }
-else
-{
-n = n * 10 - (s[i] - '0');
-i++;
+return (n);
 }
-signo *= -1;
-return (n *signo);
-}
+
