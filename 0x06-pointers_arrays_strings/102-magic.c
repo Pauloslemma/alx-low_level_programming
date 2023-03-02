357e683c88cd98a25522e498aff15edc96c7a5c6
Pauloslemma <pauloslemma1994@gmail.com>
2023-03-02 10:53:42 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,13 +2,22 @@
 
 int main(void)
 {
-int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-int *p = a;
+  int n;
+  int a[5];
+  int *p;
 
-// Write your code below this line
-printf("%d\n", *(p + 2));
-
-// Don't write anything below this line
-return 0;
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+   p[5] = 98 ;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
-
