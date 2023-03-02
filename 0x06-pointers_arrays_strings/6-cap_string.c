7189b2e3b1fe10a2af5400e90faedda34c336302
Pauloslemma <pauloslemma1994@gmail.com>
2023-03-02 10:05:41 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * cap_string - a function that capitalizes all words of a string
- * @n: input string
- * Return: caps on first letter of a separator
- */
+* cap_string - a function that capitalizes all words of a string
+* @n: input string
+* Return: caps on first letter of a separator
+*/
 char *cap_string(char *n)
 {
 int i, x;
 int cap = 32;
 int separators[] = {',', ';', '.', '?', '"',
- '(', ')', '{', '}', ' ', '\n', '\t'};
+'(', ')', '{', '}', ' ', '\n', '\t'};
 
 for (i = 0; n[i] != '\0'; i++)
 {
