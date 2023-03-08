ff621521600f34d3161c3c802d3c60ff395b5fae
Pauloslemma <pauloslemma1994@gmail.com>
2023-03-08 04:43:49 -0800

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - Entery point
+ *@s: input 
+ * return: always 0 (success)
+ */
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
@@ -7,6 +12,6 @@ _putchar('\n');
 else
 {
 _putchar(*s);
-_puts recursion(s+1);
+_puts_recursion(s + 1);
 }
 }
