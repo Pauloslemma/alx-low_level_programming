166355a886eaa1130f40eb50ebb68e2b37cb8bc2
Pauloslemma <pauloslemma1994@gmail.com>
2023-07-07 08:23:11 -0700

    function that prints a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,31 +1,32 @@
-nclude "hash_tables.h"
+#include "hash_tables.h"
 
 /**
- *  * hash_table_print - function to print the key:value from ht
- *   * @ht: pointer to hash table
- *    *
- *     */
+ * hash_table_print - function to print the key:value from ht
+ * @ht: pointer to hash table
+ *
+ */
 void hash_table_print(const hash_table_t *ht)
 {
-		unsigned long int i = 0;
-			hash_node_t  *bucket;
-				int not_fin = 0;
+	unsigned long int i = 0;
+	hash_node_t  *bucket;
+	int not_fin = 0;
 
-					if (!ht)
-								return;
+	if (!ht)
+		return;
 
-						printf("{");
-							for (i = 0; i < ht->size; i++)
-									{
-												bucket = ht->array[i];
-														while (bucket)
-																	{
-																					if (not_fin)
-																										printf(", ");
-																								printf("'%s': '%s'", bucket->key, bucket->value);
-																											not_fin = 1;
-																														bucket = bucket->next;
-																																}
-															}
-								printf("}\n");
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		bucket = ht->array[i];
+		while (bucket)
+		{
+			if (not_fin)
+				printf(", ");
+			printf("'%s': '%s'", bucket->key, bucket->value);
+			not_fin = 1;
+			bucket = bucket->next;
+		}
+	}
+	printf("}\n");
 }
+
